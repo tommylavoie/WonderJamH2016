1191510999ae650b6c1055f36ec272731e553394
tommylavoie <tom.patate@hotmail.com>
2016-03-04 17:55:33 -0500

    Plus court chemin

---
+++ WonderJamH2016/Assets/Scripts/Tommy/DijkstraCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class DijkstraCalculator
+{
+    public int[,] results = new int[Grid.NUMBER_OF_ROWS, Grid.NUMBER_OF_COLS];
+    public Position[,] paths = new Position[Grid.NUMBER_OF_ROWS, Grid.NUMBER_OF_COLS];
+
+    public int[,] GetShortestPath(Grid grid, Position start)
+    {       
+        for (int i = 0; i < Grid.NUMBER_OF_ROWS; i++)
+        {
+            for(int j=0;j< Grid.NUMBER_OF_COLS;j++)
+            {
+                results[i, j] = Int32.MaxValue;
+            }
+        }
+
+        results[start.x, start.y] = 0;
+        paths[start.x, start.y] = start;
+        if (start.x != 0 && grid.getGrid()[start.x - 1, start.y] == 1)
+            GetShortestPathRecursive(grid, new Position(start.x - 1, start.y), start);
+        if (start.x != Grid.NUMBER_OF_ROWS - 1 && grid.getGrid()[start.x + 1, start.y] == 1)
+            GetShortestPathRecursive(grid, new Position(start.x + 1, start.y), start);
+        if (start.y != 0 && grid.getGrid()[start.x, start.y - 1] == 1)
+            GetShortestPathRecursive(grid, new Position(start.x, start.y - 1), start);
+        if (start.y != Grid.NUMBER_OF_COLS - 1 && grid.getGrid()[start.x, start.y + 1] == 1)
+            GetShortestPathRecursive(grid, new Position(start.x, start.y + 1), start);
+
+        return results;
+    }
+
+    void GetShortestPathRecursive(Grid grid, Position node, Position caller)
+    {
+        if (results[node.x, node.y] > results[caller.x, caller.y])
+        {
+            results[node.x, node.y] = results[caller.x, caller.y] + 1;
+            paths[node.x, node.y] = caller;
+
+            if (node.x != 0 && grid.getGrid()[node.x - 1, node.y] == 1)
+                GetShortestPathRecursive(grid, new Position(node.x - 1, node.y), node);
+            if (node.x != Grid.NUMBER_OF_ROWS - 1 && grid.getGrid()[node.x + 1, node.y] == 1)
+                GetShortestPathRecursive(grid, new Position(node.x + 1, node.y), node);
+            if (node.y != 0 && grid.getGrid()[node.x, node.y - 1] == 1)
+                GetShortestPathRecursive(grid, new Position(node.x, node.y - 1), node);
+            if (node.y != Grid.NUMBER_OF_COLS - 1 && grid.getGrid()[node.x, node.y + 1] == 1)
+                GetShortestPathRecursive(grid, new Position(node.x, node.y + 1), node);
+        }
+    }
+}
---
+++ WonderJamH2016/Assets/Scripts/Tommy/GoalInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class GoalInfo
+{
+    Position position;
+
+    public GoalInfo(Position p)
+    {
+        this.position = p;
+    }
+
+    public Position getCellConnectedToGoal()
+    {
+        if (position.x == -1)
+        {
+            return new Position(0, position.y);
+        }
+        else if (position.x == Grid.NUMBER_OF_ROWS)
+        {
+            return new Position(Grid.NUMBER_OF_ROWS - 1, position.y);
+        }
+        else if (position.y == -1)
+        {
+            return new Position(position.x, 0);
+        }
+        else if (position.y == Grid.NUMBER_OF_COLS)
+        {
+            return new Position(position.x, Grid.NUMBER_OF_COLS - 1);
+        }
+        else
+            return null;
+    }
+}
