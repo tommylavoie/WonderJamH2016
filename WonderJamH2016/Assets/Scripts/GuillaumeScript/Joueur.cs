5459dcecec4ab416fbb1247bb26711a9e2ff511a
tommylavoie <tom.patate@hotmail.com>
2016-03-04 20:48:33 -0500

    Bullshit

---
+++ WonderJamH2016/Assets/Scripts/GuillaumeScript/Joueur.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Joueur : MonoBehaviour {
+
+
+    public int resource;
+    public int score;
+    public GameObject resourceUI;
+    public GameObject scoreUI;
+
+    RaycastHit2D hit;
+
+	// Use this for initialization
+	void Start () {
+        setScore(score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setResource(int nbrDeResource)
+    {
+        resource = nbrDeResource;
+        updaterResourceUI();
+    }
+
+    public int getResource()
+    {
+        return resource;
+    }
+
+    public void addResource(int nbrDeResourceGagner)
+    {
+        resource = resource = nbrDeResourceGagner;
+        updaterResourceUI();
+    }
+
+    public void depenserResource(int nbrDeResourceDepenser)
+    {
+        resource = resource - nbrDeResourceDepenser;
+        updaterResourceUI();
+    }
+
+    public bool PeutIlAcheter(int nbrDeResouceNecessaire)
+    {
+        if (nbrDeResouceNecessaire <= resource)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void setScore(int leScore)
+    {
+        score = leScore;
+        updaterScoreUI();
+    }
+
+    public void addScore(int leScore)
+    {
+        score = score + leScore;
+        updaterScoreUI();
+
+    }
+
+    public void updaterScoreUI()
+    {
+        scoreUI.GetComponent<Text>().text = score.ToString();
+    }
+
+    public void updaterResourceUI()
+    {
+        resourceUI.GetComponent<Text>().text = resource.ToString();
+    }
+
+    //Fonction qui permet de savoir si un objet est deja dans la case
+    public string dejaQuelqueChoseALaCase()
+    {
+        Vector2 pourRayCast = new Vector2 (0.5f,0);
+
+        hit = Physics2D.Raycast(transform.position, pourRayCast);
+
+        if(hit.collider != null)
+        {
+            return hit.transform.gameObject.tag;
+        }
+        else
+        {
+            return ("none");
+        }
+    }
+
+}
