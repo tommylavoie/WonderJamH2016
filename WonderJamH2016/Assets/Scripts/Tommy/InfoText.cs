63b55620ba57cfa02f06320e30edb26d2ec7999d
tommylavoie <tom.patate@hotmail.com>
2016-03-05 18:12:16 -0500

    Start infotext

---
+++ WonderJamH2016/Assets/Scripts/Tommy/InfoText.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InfoText : MonoBehaviour
+{
+    public float Speed = 1;
+
+    Queue<string> infoQueue;
+    string actual = "";
+    bool showing = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        infoQueue = new Queue<string>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(!showing && infoQueue.Count > 0)
+            StartShow();
+        if(showing)
+            ExecuteShow();
+	}
+
+    void StartShow()
+    {
+        actual = infoQueue.Dequeue();
+        showing = true;
+    }
+
+    void ExecuteShow()
+    {
+
+    }
+
+    void EndShow()
+    {
+
+    }
+}
