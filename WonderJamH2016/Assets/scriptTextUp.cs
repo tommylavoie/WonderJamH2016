a91b192f4222f3d602c62004285bbb79338ab3c7
antronik7 <antoineberthiaumedutil@gmail.com>
2016-03-05 14:52:53 -0500

    Text Up

---
+++ WonderJamH2016/Assets/scriptTextUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptTextUp : MonoBehaviour {
+
+    string myText;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setText(string text)
+    {
+        GetComponent<TextMesh>().text = text;
+    }
+}
