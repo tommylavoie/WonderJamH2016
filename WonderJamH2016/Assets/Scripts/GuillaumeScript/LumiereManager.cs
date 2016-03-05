6ab97e434b6d4c1ad2dad44980f08727f6a98fed
antronik7 <antoineberthiaumedutil@gmail.com>
2016-03-05 11:19:18 -0500

    Arranger lumiere manager

--- WonderJamH2016/Assets/Scripts/GuillaumeScript/LumiereManager.cs
+++ WonderJamH2016/Assets/Scripts/GuillaumeScript/LumiereManager.cs
@@ -1,27 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LumiereManager : MonoBehaviour {
+public class LumiereManager : MonoBehaviour
+{
 
-   int grosseurTableau;
-   public Lumiere[] poolDesLumiere;
-   public GameObject[] poolDesLumiereGameObject;
-   int compteur = 0;
-   public Grid laGrid;
+    int grosseurTableau;
+    public Lumiere[] poolDesLumiere;
+    public GameObject[] poolDesLumiereGameObject;
+    int compteur = 0;
+    public Grid laGrid;
 
-   public GameObject laLumiere;
+    public Joueur joueur1;
+    public Joueur joueur2;
 
-	// Use this for initialization
-	void Start ()
+    public GameObject laLumiere;
+
+    // Use this for initialization
+    void Start()
     {
         initialiserMonTableau(laGrid.getSpawners());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     public void initialiserMonTableau(Position[] leTableau)
     {
@@ -32,25 +36,24 @@ public class LumiereManager : MonoBehaviour {
 
         for (int i = 0; i < leTableau.Length; i++)
         {
-            GameObject tata = (GameObject)Instantiate(laLumiere, Vector2.zero, Quaternion.identity);
-            poolDesLumiereGameObject[compteur] = tata;
-            Lumiere lumiere = poolDesLumiereGameObject[compteur].GetComponent<Lumiere>();
-            lumiere.initialiserLumiere(leTableau[i].x - 1, leTableau[i].y, laGrid);
+
+            poolDesLumiereGameObject[compteur] = (GameObject)Instantiate(laLumiere, Vector2.zero, Quaternion.identity);
+            poolDesLumiereGameObject[compteur].GetComponent<Lumiere>().initialiserLumiere(leTableau[i].x - 1, leTableau[i].y, laGrid, joueur1, joueur2);
 
             compteur++;
 
             poolDesLumiereGameObject[compteur] = (GameObject)Instantiate(laLumiere, Vector2.zero, Quaternion.identity);
-            poolDesLumiereGameObject[compteur].GetComponent<Lumiere>().initialiserLumiere(leTableau[i].x, leTableau[i].y + 1, laGrid);
+            poolDesLumiereGameObject[compteur].GetComponent<Lumiere>().initialiserLumiere(leTableau[i].x, leTableau[i].y + 1, laGrid, joueur1, joueur2);
 
             compteur++;
 
             poolDesLumiereGameObject[compteur] = (GameObject)Instantiate(laLumiere, Vector2.zero, Quaternion.identity);
-            poolDesLumiereGameObject[compteur].GetComponent<Lumiere>().initialiserLumiere(leTableau[i].x + 1, leTableau[i].y, laGrid);
+            poolDesLumiereGameObject[compteur].GetComponent<Lumiere>().initialiserLumiere(leTableau[i].x + 1, leTableau[i].y, laGrid, joueur1, joueur2);
 
             compteur++;
 
             poolDesLumiereGameObject[compteur] = (GameObject)Instantiate(laLumiere, Vector2.zero, Quaternion.identity);
-            poolDesLumiereGameObject[compteur].GetComponent<Lumiere>().initialiserLumiere(leTableau[i].x, leTableau[i].y - 1, laGrid);
+            poolDesLumiereGameObject[compteur].GetComponent<Lumiere>().initialiserLumiere(leTableau[i].x, leTableau[i].y - 1, laGrid, joueur1, joueur2);
 
             compteur++;
 
@@ -60,8 +63,7 @@ public class LumiereManager : MonoBehaviour {
 
     public void unTic()
     {
-        Debug.Log("Je suis dans le tic");
-        for(int i = 0; i < poolDesLumiere.Length; i++)
+        for (int i = 0; i < poolDesLumiereGameObject.Length; i++)
         {
             poolDesLumiereGameObject[i].GetComponent<Lumiere>().updateTaPosition();
         }
