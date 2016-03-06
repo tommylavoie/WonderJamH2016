4a20f0ecf5c46c4d93e0516d04f2a58957836023
antronik7 <antoineberthiaumedutil@gmail.com>
2016-03-06 06:56:01 -0500

    canada

---
+++ WonderJamH2016/Assets/CANADA.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CANADA : MonoBehaviour {
+
+    public AudioClip canada;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void playCanada()
+    {
+        transform.position = new Vector3(transform.position.x, -10f, -10f);
+        GetComponent<AudioSource>().PlayOneShot(canada, 1f);
+        StartCoroutine(Wait(5));
+    }
+
+    IEnumerator Wait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
+}
