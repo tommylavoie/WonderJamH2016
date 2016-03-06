ebd8fe08dae8b30ed7a4ca9c6317481e5a729213
Domo14 <guillaumelaplante14@gmail.com>
2016-03-06 05:01:36 -0500

    Le hurry up !

---
+++ WonderJamH2016/Assets/Scripts/GuillaumeScript/PourLeSpeedUp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PourLeSpeedUp : MonoBehaviour {
+
+    public AudioClip hurryUp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void sonHurryUp()
+    {
+        GetComponent<AudioSource>().PlayOneShot(hurryUp, 0.8f);
+
+    }
+}
